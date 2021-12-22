13da9424361e8e696f7b48bf5a081595150ec357
Yujin <yujinyll9148@gmail.com>
2021-12-23 06:17:53 +0900

    Update index.js -CORS

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const bodyParser = require('body-parser');
 const PORT = process.env.port || 8080;
 const cors = require('cors');
 
+const whitelist = ["https://www.gamsungsoft.com","http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not Allowed Origin!"));
+    }
+  },
+};
+
 const db = mysql.createPool({
   host: '3.130.190.15',
   user: 'user',
@@ -12,7 +23,7 @@ const db = mysql.createPool({
   database: 'gamsung'
 });
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
